b751df52782187cde19c1891d146320aa50d00b0
woodapples <marco.holzeapfel@protonmail.com>
2023-11-02 19:46:06 +0100

    Geld wurde erstellt

--- code/src/Main.java
+++ code/src/Main.java
@@ -1,5 +1,17 @@
+import dda.Geld;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Marco!");
+
+
+
+
+        Geld g1 = new Geld();
+        Geld g2 = new Geld();
+        Geld g3 = new Geld(15, "Koks", 1, 2);
+
+
+        System.out.println(g1.toString());
+        System.out.println(g2.toString());
+        System.out.println(g3.toString());
     }
 }
